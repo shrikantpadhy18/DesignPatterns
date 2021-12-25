9e31d4494f20b5ffe773a3ffb672de4493d4060d
unknown <shrikantpadhy18@gmail.com>
2021-12-25 18:58:36 +0530

    singleton design pattern

--- creationaldesignpattern/SingletonDesignPattern/Singleton.java
+++ creationaldesignpattern/SingletonDesignPattern/Singleton.java
@@ -7,15 +7,20 @@ class SingleTon{
 }
 
 class SingleTonDemo{
+    //static members are created only once that too when the class gets loaded in the memory ..so now it wont matter how many times the getInstance is invoked ..there would be only one object
     private static SingleTonDemo obj=new SingleTonDemo();
 
     private SingleTonDemo(){
-
+        //by creating a private constructor..we have actually made sure that java will not be providing us the default public constructor that could be invoked outside of this class.
     }
 
     public static SingleTonDemo getInstance(){
         
         return(obj);
+    }
+
+    public void toDo(){
+        //our task
     }
 
 }
